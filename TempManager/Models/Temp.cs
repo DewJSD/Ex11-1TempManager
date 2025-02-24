a4a2aca71c68ba19e95f5e15b489092429fde548
DewJSD <jeffreywdew@gmail.com>
2025-02-23 22:49:52 -0600

    Through step 14

--- TempManager/Models/Temp.cs
+++ TempManager/Models/Temp.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TempManager.Models
 {
     public class Temp
     {
         public int Id { get; set; }
+
+        [Required]
         public DateTime? Date { get; set; }
+
+        [Required]
+        [Range(-200.0, 200.0)]
         public double? Low { get; set; }
+
+        [Required]
+        [Range(-200.0, 200.0)]
         public double? High { get; set; }
     }
 }
